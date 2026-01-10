156bd36c6885eb6fa1d20707ace2f139855ab820
SamirShef <ssserin10@gmail.com>
2026-01-10 16:55:53 +0300

    Add RetType to ASTVisitor templates

--- Include/onyx/AST/Visitor.h
+++ Include/onyx/AST/Visitor.h
@@ -2,27 +2,22 @@
 #include <onyx/AST/AST.h>
 
 namespace onyx {
-    template<typename Derived>
+    template<typename Derived, typename RetType = void>
     class ASTVisitor {
     public:
-        void
+        RetType
         visit(Node *node) {
             switch (node->GetKind()) {
                 case NkVarDeclStmt:
-                    static_cast<Derived*>(this)->visitVarDeclStmt(static_cast<VarDeclStmt*>(node));
-                    break;
+                    return static_cast<Derived*>(this)->visitVarDeclStmt(static_cast<VarDeclStmt*>(node));
                 case NkBinaryExpr:
-                    static_cast<Derived*>(this)->visitBinaryExpr(static_cast<BinaryExpr*>(node));
-                    break;
+                    return static_cast<Derived*>(this)->visitBinaryExpr(static_cast<BinaryExpr*>(node));
                 case NkUnaryExpr:
-                    static_cast<Derived*>(this)->visitUnaryExpr(static_cast<UnaryExpr*>(node));
-                    break;
+                    return static_cast<Derived*>(this)->visitUnaryExpr(static_cast<UnaryExpr*>(node));
                 case NkVarExpr:
-                    static_cast<Derived*>(this)->visitVarExpr(static_cast<VarExpr*>(node));
-                    break;
+                    return static_cast<Derived*>(this)->visitVarExpr(static_cast<VarExpr*>(node));
                 case NkLiteralExpr:
-                    static_cast<Derived*>(this)->visitLiteralExpr(static_cast<LiteralExpr*>(node));
-                    break;
+                    return static_cast<Derived*>(this)->visitLiteralExpr(static_cast<LiteralExpr*>(node));
                 default: {}
             }
         }
