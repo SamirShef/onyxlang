ed194fa852d4deebd2a61c549b0663e28fdb723a
SamirShef <ssserin10@gmail.com>
2026-01-11 14:25:52 +0300

    Added code generation and linkage to executable

--- Include/onyx/Basic/DiagnosticEngine.h
+++ Include/onyx/Basic/DiagnosticEngine.h
@@ -4,13 +4,25 @@
 namespace onyx {
     class DiagnosticEngine {
         llvm::SourceMgr &_srcMgr;
+        bool _hasErrors = false;
 
     public:
         explicit DiagnosticEngine(llvm::SourceMgr &mgr) : _srcMgr(mgr) {}
 
         DiagnosticBuilder
         Report(llvm::SMLoc loc, DiagKind kind) {
+            _hasErrors = true;
             return DiagnosticBuilder(_srcMgr, loc, GetDiagInfo(kind));
+        }
+
+        bool
+        HasErrors() const {
+            return _hasErrors;
+        }
+
+        void
+        ResetErrors() {
+            _hasErrors = false;
         }
     };
 }
